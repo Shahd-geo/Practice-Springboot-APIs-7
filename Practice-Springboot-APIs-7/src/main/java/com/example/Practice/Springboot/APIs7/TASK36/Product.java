499ad186670c0875ab9934532e1bab309a575538
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 15:25:29 +0400

    ADD Getter methods for all fields

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/Product.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/Product.java
@@ -1,5 +1,10 @@
 package com.example.Practice.Springboot.APIs7.TASK36;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Product {
     private int productId;
     private String productName;
