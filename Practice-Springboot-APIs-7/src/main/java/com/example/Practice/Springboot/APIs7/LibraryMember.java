02c0b033f609e7ac3c7c28fedd3d4540f5b81492
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 14:54:52 +0400

    CREATE GETTER AND SETTER USING lombok

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/LibraryMember.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/LibraryMember.java
@@ -1,7 +1,13 @@
 package com.example.Practice.Springboot.APIs7;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class LibraryMember {
     private int memberId;
     private String fullName;
     private String membershipType  ;
+
 }
