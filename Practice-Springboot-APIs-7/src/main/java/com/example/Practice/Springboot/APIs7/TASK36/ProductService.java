19b931feeeca8b2705dfc8efe2101cbfb825b42c
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 15:31:44 +0400

    Create Declare a private HashMap

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/ProductService.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/ProductService.java
@@ -1,4 +1,7 @@
 package com.example.Practice.Springboot.APIs7.TASK36;
 
+import java.util.HashMap;
+
 public class ProductService {
+    private HashMap<Integer, Product> productMap=new HashMap<>();
 }
