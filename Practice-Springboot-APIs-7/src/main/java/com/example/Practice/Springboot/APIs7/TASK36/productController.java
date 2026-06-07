e54d9ab04f288177fbcdf1d3292b8f3e1cdd4ac5
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 15:48:46 +0400

    Done test

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/productController.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/productController.java
@@ -1,9 +1,21 @@
 package com.example.Practice.Springboot.APIs7.TASK36;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class productController {
+    private ProductService productService;
+    public productController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
     @DeleteMapping("/products/{id}")
+    public String deleteProduct(@PathVariable int id) {
+
+        return productService.deleteProductById(id);
+
+    }
 }
