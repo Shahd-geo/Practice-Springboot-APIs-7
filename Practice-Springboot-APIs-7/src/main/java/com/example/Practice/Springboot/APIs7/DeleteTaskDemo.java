f6c947f72a6f15c901c942e421fe49551afd90b6
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 14:35:42 +0400

    Create Task Collection

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/DeleteTaskDemo.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/DeleteTaskDemo.java
@@ -1,4 +1,12 @@
 package com.example.Practice.Springboot.APIs7;
 
+import java.util.ArrayList;
+
 public class DeleteTaskDemo {
+    public static void deleteTask() {
+    ArrayList<String> tasks = new ArrayList<>();
+     tasks.add("Buy groceries");
+     tasks.add("Call doctor");
+     tasks.add("Finish report");
+     System.out.println("Initial Task List:" + tasks);
 }
