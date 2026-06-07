335164f592bbf48e19311a91a86a88ba13089669
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-07 15:41:56 +0400

    Create DELETE Endpoint

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/productController.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK36/productController.java
@@ -1,7 +1,9 @@
 package com.example.Practice.Springboot.APIs7.TASK36;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class productController {
+    @DeleteMapping("/products/{id}")
 }
