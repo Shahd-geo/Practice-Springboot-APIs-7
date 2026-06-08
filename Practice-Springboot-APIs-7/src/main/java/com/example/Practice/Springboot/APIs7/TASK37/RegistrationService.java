46a9c2b57c1dd770efdf2d586f50b629075e1a2d
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-08 07:42:07 +0400

    add sample data

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/RegistrationService.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/RegistrationService.java
@@ -1,4 +1,15 @@
 package com.example.Practice.Springboot.APIs7.TASK37;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistrationService {
+    private Map<Integer, EventRegistration> registrations=new HashMap<>();
+    public void loadSample (){
+        registrations.put(123,new EventRegistration(123,"Ahmad"));
+        registrations.put(102, new EventRegistration(102, "Sara"));
+        registrations.put(103, new EventRegistration(103, "Mona"));
+        registrations.put(104, new EventRegistration(104, "Ali"));
+    }
+
 }
