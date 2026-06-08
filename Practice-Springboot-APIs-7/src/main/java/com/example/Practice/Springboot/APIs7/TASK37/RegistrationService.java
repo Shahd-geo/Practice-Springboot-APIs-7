a795430c2d0b118aec10fbb836f6e1ef0780dcb0
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-08 07:46:57 +0400

    . Implement Bulk Delete Method

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/RegistrationService.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/RegistrationService.java
@@ -1,15 +1,22 @@
 package com.example.Practice.Springboot.APIs7.TASK37;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@Service
 public class RegistrationService {
     private Map<Integer, EventRegistration> registrations=new HashMap<>();
-    public void loadSample (){
+    //initialize data inside Constructor
+    public RegistrationService() {
         registrations.put(123,new EventRegistration(123,"Ahmad"));
         registrations.put(102, new EventRegistration(102, "Sara"));
         registrations.put(103, new EventRegistration(103, "Mona"));
         registrations.put(104, new EventRegistration(104, "Ali"));
+    }
+    public String deleteMultipleByIds(List<Integer> ids){
+
     }
 
 }
