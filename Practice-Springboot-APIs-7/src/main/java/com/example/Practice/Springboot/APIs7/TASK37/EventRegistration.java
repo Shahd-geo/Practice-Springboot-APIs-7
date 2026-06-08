ee05d5c5b21b98d718f8017428aa22b8933c6580
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-08 07:15:06 +0400

    creat Getter and (Setter using lombok

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/EventRegistration.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/EventRegistration.java
@@ -1,5 +1,12 @@
 package com.example.Practice.Springboot.APIs7.TASK37;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class EventRegistration {
     private  int registrationId;
     private  String attendeeNam;
