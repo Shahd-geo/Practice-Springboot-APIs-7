64d9b0df5e677f88e113bb3b735fdd37d3ab88b1
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-06-08 07:51:44 +0400

    If registration exists

--- Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/RegistrationService.java
+++ Practice-Springboot-APIs-7/src/main/java/com/example/Practice/Springboot/APIs7/TASK37/RegistrationService.java
@@ -20,6 +20,9 @@ public class RegistrationService {
         List<Integer> deletedIds = new ArrayList<>();
         List<Integer> notFoundIds = new ArrayList<>();
         for(Integer id:ids){
+            if (registrations.containsKey(id)) {
+                registrations.remove(id);
+                deletedIds.add(id);
 
         }
     }
